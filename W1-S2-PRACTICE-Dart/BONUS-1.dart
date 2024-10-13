8571b5d3a070a5543b01102c7ecd83d0c95bc97b
ehak-mat <ehakmath@gmail.com>
2024-10-13 21:49:40 +0700

    bonus-1

--- W1-S2-PRACTICE-Dart/BONUS-1.dart
+++ W1-S2-PRACTICE-Dart/BONUS-1.dart
@@ -1,5 +1,5 @@
 enum Direction { north, east, south, west }
- 
+
 void main() {
   // Initial position {7, 3} and facing north
   int x = 7;
@@ -9,10 +9,64 @@ void main() {
   // Example instruction sequence
   const instructions = "RAALAL";
 
-  // Process the instructions and get the final position and direction
-  
+  // Function to turn right
+  Direction turnRight(Direction dir) {
+    switch (dir) {
+      case Direction.north:
+        return Direction.east;
+      case Direction.east:
+        return Direction.south;
+      case Direction.south:
+        return Direction.west;
+      case Direction.west:
+        return Direction.north;
+    }
+  }
+
+  // Function to turn left
+  Direction turnLeft(Direction dir) {
+    switch (dir) {
+      case Direction.north:
+        return Direction.west;
+      case Direction.east:
+        return Direction.north;
+      case Direction.south:
+        return Direction.east;
+      case Direction.west:
+        return Direction.south;
+    }
+  }
+
+  // Function to advance in the current direction
+  void advance() {
+    switch (direction) {
+      case Direction.north:
+        y += 1;
+        break;
+      case Direction.east:
+        x += 1;
+        break;
+      case Direction.south:
+        y -= 1;
+        break;
+      case Direction.west:
+        x -= 1;
+        break;
+    }
+  }
+
+  // Process the instructions
+  for (var command in instructions.split('')) {
+    if (command == 'R') {
+      direction = turnRight(direction);
+    } else if (command == 'L') {
+      direction = turnLeft(direction);
+    } else if (command == 'A') {
+      advance();
+    }
+  }
 
   // Print the final position and direction
-  print("Final position:  ");
-  print("Facing:  ");
+  print("Final position: {$x, $y}");
+  print("Facing: $direction");
 }
