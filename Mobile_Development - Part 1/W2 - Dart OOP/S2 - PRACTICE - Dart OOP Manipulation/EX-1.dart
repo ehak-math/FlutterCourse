b803418e92418e585311f76dab3ad8dd3ce98b0e
ehak-mat <ehakmath@gmail.com>
2024-10-22 20:38:16 +0700

    S2 - Practice

--- Mobile_Development - Part 1/W2 - Dart OOP/S2 - PRACTICE - Dart OOP Manipulation/EX-1.dart
+++ Mobile_Development - Part 1/W2 - Dart OOP/S2 - PRACTICE - Dart OOP Manipulation/EX-1.dart
@@ -1,20 +1,107 @@
-enum Skill { FLUTTER, DART, OTHER }
+// list of skill
+enum Skill { FLUTTER, DART, OHTER }
 
+// class employee
 class Employee {
-  String name;
-  double baseSalary;
+  // attribute of variable declare with private
+  final String _name;
+  final double _baseSalary;
 
-  Employee(this.name, this.baseSalary);
+  // new attribute in class Employee
+  final int _yearOfExperience;
 
-  void printDetails() {
-    print('Employee: $name, Base Salary: \$${baseSalary}');
+  // a class Address
+  final Address _address;
+
+  // a list of Skill
+  final List<Skill> _skills;
+
+  // constructore for class Employee
+  Employee(this._name, this._baseSalary, this._yearOfExperience, this._address,
+      this._skills);
+
+  // update the constructor specific types of class Employee work with DART
+  Employee.MobileDeveloperWithDART(
+    this._name,
+    this._baseSalary,
+    this._yearOfExperience,
+    this._address,
+  ) : _skills = [Skill.DART];
+
+  // update the constructor specific types of class Employee work with DART
+  Employee.MobileDeveloperWithFlutter(
+    this._name,
+    this._baseSalary,
+    this._yearOfExperience,
+    this._address,
+  ) : _skills = [Skill.FLUTTER];
+
+  // make all attribute private and provide getter method for accessing
+  String get name => _name;
+  double get baseSalary => _baseSalary;
+  int get yearOfExperience => _yearOfExperience;
+  Address get address => _address;
+  List<Skill> get skill => _skills;
+
+  // the method computer salary
+  double computerSalary() {
+    double salary = _baseSalary;
+    salary += _yearOfExperience * 2000;
+    for (var skill in _skills) {
+      salary += _getBonus(skill);
+    }
+    return salary;
+  }
+
+  // method bonus
+  double _getBonus(Skill skill) {
+    switch (skill) {
+      case Skill.FLUTTER:
+        return 5000;
+      case Skill.DART:
+        return 3000;
+      case Skill.OHTER:
+        return 1000;
+      default:
+        return 0;
+    }
+  }
+
+// method to display employee's information
+  void displayResult() {
+    print("Employee Name: $name");
+    print("Base Salary: \$${baseSalary}");
+    print("Years of Experience: $yearOfExperience");
+    print(
+        "Address: ${_address._street}, ${_address._city} ${_address._zipCode}");
+    print(
+        "Skills: ${_skills.map((skill) => skill.toString().split('.').last).join(', ')}");
+    computerSalary();
   }
 }
 
-void main() {
-  var emp1 = Employee('Sokea', 40000);
-  emp1.printDetails();
+// class Address
+class Address {
+  // attribute of variable declare with private
+  final String _city;
+  final String _street;
+  final String _zipCode;
 
-  var emp2 = Employee('Ronan', 45000);
-  emp2.printDetails();
+// constructor of class Address
+  Address(this._city, this._street, this._zipCode);
+}
+
+// method main
+main() {
+  // object for Employee skill Mobile Developer work with DART
+  Address address1 = Address('KompongChnang', '7F', '19875E');
+  Employee employeeWorkDart =
+      Employee.MobileDeveloperWithDART('Pich Rong', 9000, 7, address1);
+  employeeWorkDart.displayResult();
+
+  // object for Employee skill Mobile Developer work with FLUTTER
+  Address address2 = Address('Phnom Penh', '9A', '65452ED');
+  Employee employeeWorkFlutter =
+      Employee.MobileDeveloperWithFlutter('Math Ehak', 5000, 4, address2);
+  employeeWorkFlutter.displayResult();
 }
